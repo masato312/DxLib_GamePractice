fef25e64ed5c15e2b0489e04b92705ca80edd2db
masato312 <138412963+masato312@users.noreply.github.com>
2023-10-16 14:29:57 +0900

    RankingScene

---
+++ gameprograming/tetorissss/GameMainScene.cpp
@@ -0,0 +1,61 @@
+#include "GameMainScene.h"
+#include "DxLib.h"
+#include "Block.h"
+#include "SceneManager.h"
+
+int BackGround_image;
+int BackGround_sound;
+int GameOver_sound;
+int Score;
+
+int GameMainScene_Initialize(void)
+{
+	int ret = 0;
+
+	ret = Block_Initialize();
+	BackGround_image = LoadGraph("images/stage.png");
+	BackGround_sound = LoadSoundMem("sounds/BGM017.ogg");
+	GameOver_sound = LoadSoundMem("sounds/GameOver.mp3");
+
+	if (BackGround_image == -1)
+	{
+		ret = -1;
+	}
+	if (BackGround_sound == -1)
+	{
+		ret = -1;
+	}
+	return ret;
+}
+
+void GameMainScene_Update(void)
+{
+	PlaySoundMem(BackGround_sound, DX_PLAYTYPE_LOOP, FALSE);
+
+	Block_Update();
+
+	Score = Get_Line() * 50;
+
+	if (Get_GenerateFlg() != TRUE)
+	{
+		PlaySoundMem(GameOver_sound, DX_PLAYTYPE_BACK, FALSE);
+		Change_Scene(E_RANKING);
+		StopSoundMem(BackGround_sound);
+	}
+}
+
+void GameMainScene_Drow(void)
+{
+	DrawGraph(0, 0, BackGround_image, TRUE);
+
+	Block_Draw();
+	SetFontSize(100);
+
+	DrawFormatString(800, 100, GetColor(255, 255, 255), "%d", Score);
+	SetFontSize(20);
+}
+
+int Get_Score(void)
+{
+	return Score;
+}
---
+++ gameprograming/tetorissss/InputControl.cpp
@@ -0,0 +1,114 @@
+#include "InputControl.h"
+#include "DxLib.h"
+
+#define XINPUT_BUTTON_MAX (16)
+
+enum KEY_STATE
+{
+	E_NONE,
+	E_CLICK,
+	E_PRESS,
+	E_RELEASED,
+	E_KEY_STATE_MAX
+};
+
+KEY_STATE button_state[XINPUT_BUTTON_MAX];
+
+void InputControl_Initialize(void)
+{
+	int i;
+
+	for (i = 0; i < XINPUT_BUTTON_MAX; i++)
+	{
+		button_state[i] = E_NONE;
+	}
+}
+
+void InputControl_Update(void)
+{
+	int i;
+	XINPUT_STATE input_controller;
+
+	GetJoypadXInputState(DX_INPUT_PAD1, &input_controller);
+
+	for (i = 0; i < XINPUT_BUTTON_MAX; i++)
+	{
+		if (input_controller.Buttons[i] == TRUE)
+		{
+			switch (button_state[i])
+			{
+			case E_NONE:
+			case E_RELEASED:
+				button_state[i] = E_CLICK;
+				break;
+			case E_CLICK:
+			case E_PRESS:
+				break;
+			default:
+				button_state[i] = E_NONE;
+				break;
+			}
+		}
+		else
+		{
+			switch (button_state[i])
+			{
+			case E_NONE:
+			case E_RELEASED:
+				button_state[i] = E_NONE;
+				break;
+			case E_CLICK:
+			case E_PRESS:
+				button_state[i] = E_RELEASED;
+				break;
+			default:
+				button_state[i] = E_NONE;
+				break;
+			}
+		}
+	}
+}
+
+int GetButton(int button)
+{
+	int ret = FALSE;
+	
+	if (button_state[button] == E_CLICK || button_state[button] == E_PRESS)
+	{
+		ret = TRUE;
+	}
+	return ret;
+}
+
+int GetButtonDown(int button)
+{
+	int ret = FALSE;
+
+	if (button_state[button] == E_CLICK)
+	{
+		ret = TRUE;
+	}
+	return ret;
+}
+
+int GetButtonDown(int button)
+{
+	int ret = FALSE;
+
+	if (button_state[button] == E_RELEASED)
+	{
+		ret = TRUE;
+	}
+	return ret;
+}
+
+int GetExitButton(void)
+{
+	int ret = FALSE;
+	if ((GetButtonDown(XINPUT_BUTTON_BACK) == TRUE) || (CheckHitKey(KEY_INPUT_ESCAPE) ==
+		TRUE))
+	{
+		ret = TRUE;
+	}
+	return ret;
+}
